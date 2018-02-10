a1c497a98507b769bf2e613301e3210c0c4bba6c
Bratok3000 <Bratok3000@gmail.com>
2018-02-10 16:20:16 +0200

    first task finnaly finished

--- additionaltask/index.js
+++ additionaltask/index.js
@@ -1,6 +1,19 @@
-function red(){
+function red() {
     var paragraph = document.getElementsByTagName("p");
-    for(var p = 0; p < paragraph.length; p++){
-        paragraph[p].style.border = "red";
+    for (var p = 0; p < paragraph.length; p++) {
+        paragraph[p].setAttribute("style", "background-color :red;");
     }
+}
+
+function green(){
+    var a = document.getElementsByTagName("a");
+    for(var i = 0; i < a.length; i++){
+        // a[i].setAttribute("style", "color: green");
+        a[i].setAttribute("style", "border: 1px solid green");
+    }
+}
+
+function div(){
+    var div = document.getElementById("test_div");
+    div.setAttribute("style", "color: #c34e84");
 }
